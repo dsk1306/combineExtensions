44cbca5072bb9cf86d3f5629380d7678f2ffbdff
Dmitry Kuznetsov <dsk1306@icloud.com>
2021-10-01 13:22:38 +0300

    Added assignWithOwnership and assignToMany operators (#5)

---
+++ Sources/Utilities/ObjectOwnership.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+/// The ownership of an object.
+/// - reference: [Automatic Reference Counting](https://docs.swift.org/swift-book/LanguageGuide/AutomaticReferenceCounting.html#ID52)
+public enum ObjectOwnership {
+
+    /// Keep a strong hold of the object, preventing ARC from disposing it until its released or has no references.
+    case strong
+
+    /// Weakly owned. Does not keep a strong hold of the object, allowing ARC to dispose it even if its referenced.
+    case weak
+
+    /// Unowned. Similar to weak, but implicitly unwrapped so may crash if the object is released beore being accessed.
+    case unowned
+
+}
