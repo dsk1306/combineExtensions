85aee866f4670025f86e136c11ac7637ced16b4c
Dmitry Kuznetsov <dsk1306@icloud.com>
2021-10-14 23:50:26 +0300

    ref: Removed unused paramForLinter from ResultBuilder

--- Sources/Utilities/ResultBuilder.swift
+++ Sources/Utilities/ResultBuilder.swift
@@ -3,8 +3,6 @@ import Foundation
 @resultBuilder
 public struct ResultBuilder<T> {
 
-    let paramForLinter = false
-
     public static func buildBlock(_ content: T...) -> [T] {
         content
     }
