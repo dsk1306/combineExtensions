a2f31ab53ed930b830b069dbe1db6ab88692e790
Dmitry Kuznetsov <dsk1306@icloud.com>
2021-10-14 23:29:25 +0300

    Fixed access modifiers (#9)

--- Sources/Utilities/ResultBuilder.swift
+++ Sources/Utilities/ResultBuilder.swift
@@ -1,23 +1,23 @@
 import Foundation
 
 @resultBuilder
-struct ResultBuilder<T> {
+public struct ResultBuilder<T> {
 
     let paramForLinter = false
 
-    static func buildBlock(_ content: T...) -> [T] {
+    public static func buildBlock(_ content: T...) -> [T] {
         content
     }
 
-    static func buildIf(_ content: T?) -> T? {
+    public static func buildIf(_ content: T?) -> T? {
         content
     }
 
-    static func buildEither(first: T) -> T {
+    public static func buildEither(first: T) -> T {
         first
     }
 
-    static func buildEither(second: T) -> T {
+    public static func buildEither(second: T) -> T {
         second
     }
 
