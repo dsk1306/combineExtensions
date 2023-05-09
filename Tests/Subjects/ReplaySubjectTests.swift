f4ed7612f1ba192f29d5a49c7e2379f6fa089ec5
Dmitry Kuznetsov <dsk1306@icloud.com>
2023-05-09 20:26:41 +0200

    Cleaned up ReplaySubjectTests

--- Tests/Subjects/ReplaySubjectTests.swift
+++ Tests/Subjects/ReplaySubjectTests.swift
@@ -7,35 +7,40 @@ final class ReplaySubjectTests: XCTestCase {
     // MARK: - Properties
     
     private var demandSubscription: Subscription!
-    private var subscriptions = Set<AnyCancellable>()
+    private var cancellable = CombineCancellable()
+    private var results = [Int]()
+
+    // MARK: - Base Class
+
+    override func setUp() {
+        super.setUp()
+
+        demandSubscription = nil
+        cancellable = .init()
+        results = []
+    }
     
     // MARK: - Tests
     
     func test_replaysNoValues() {
-        let subject = ReplaySubject<Int, Never>(bufferSize: 1)
-        
-        var results = [Int]()
-        
-        subject
-            .sink(receiveValue: { results.append($0) })
-            .store(in: &subscriptions)
+        ReplaySubject<Int, Never>(bufferSize: 1)
+            .sink { [weak self] in self?.results.append($0) }
+            .store(in: cancellable)
         
         XCTAssertTrue(results.isEmpty)
     }
     
     func test_missedValue_emptyBuffer() {
         let subject = ReplaySubject<Int, Never>(bufferSize: 0)
-        
+
         subject.send(1)
-        
-        var results = [Int]()
-        
+
         subject
-            .sink(receiveValue: { results.append($0) })
-            .store(in: &subscriptions)
-        
+            .sink { [weak self] in self?.results.append($0) }
+            .store(in: cancellable)
+
         subject.send(2)
-        
+
         XCTAssertEqual(results, [2])
     }
     
@@ -44,11 +49,9 @@ final class ReplaySubjectTests: XCTestCase {
         
         subject.send(1)
         
-        var results = [Int]()
-        
         subject
-            .sink(receiveValue: { results.append($0) })
-            .store(in: &subscriptions)
+            .sink { [weak self] in self?.results.append($0) }
+            .store(in: cancellable)
         
         subject.send(2)
         
@@ -63,11 +66,9 @@ final class ReplaySubjectTests: XCTestCase {
         subject.send(3)
         subject.send(4)
         
-        var results = [Int]()
-        
         subject
-            .sink(receiveValue: { results.append($0) })
-            .store(in: &subscriptions)
+            .sink { [weak self] in self?.results.append($0) }
+            .store(in: cancellable)
         
         subject.send(5)
         
@@ -80,11 +81,9 @@ final class ReplaySubjectTests: XCTestCase {
         subject.send(1)
         subject.send(2)
         
-        var results = [Int]()
-        
         subject
-            .sink(receiveValue: { results.append($0) })
-            .store(in: &subscriptions)
+            .sink { [weak self] in self?.results.append($0) }
+            .store(in: cancellable)
         
         subject.send(3)
         
@@ -102,16 +101,16 @@ final class ReplaySubjectTests: XCTestCase {
         var results3 = [Int]()
         
         subject
-            .sink(receiveValue: { results1.append($0) })
-            .store(in: &subscriptions)
+            .sink { results1.append($0) }
+            .store(in: cancellable)
         
         subject
-            .sink(receiveValue: { results2.append($0) })
-            .store(in: &subscriptions)
+            .sink { results2.append($0) }
+            .store(in: cancellable)
         
         subject
-            .sink(receiveValue: { results3.append($0) })
-            .store(in: &subscriptions)
+            .sink { results3.append($0) }
+            .store(in: cancellable)
         
         subject.send(3)
         
@@ -141,21 +140,21 @@ final class ReplaySubjectTests: XCTestCase {
                 receiveCompletion: { completions1.append($0) },
                 receiveValue: { results1.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject
             .sink(
                 receiveCompletion: { completions2.append($0) },
                 receiveValue: { results2.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject
             .sink(
                 receiveCompletion: { completions3.append($0) },
                 receiveValue: { results3.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject.send(3)
         
@@ -170,20 +169,20 @@ final class ReplaySubjectTests: XCTestCase {
     }
     
     func test_errorWithMultipleSubscribers() {
-        let subject = ReplaySubject<Int, AnError>(bufferSize: 3)
+        let subject = ReplaySubject<Int, TestError>(bufferSize: 3)
         
         subject.send(1)
         subject.send(2)
-        subject.send(completion: .failure(.someError))
+        subject.send(completion: .failure(.test))
         
         var results1 = [Int]()
-        var completions1 = [Subscribers.Completion<AnError>]()
+        var completions1 = [Subscribers.Completion<TestError>]()
         
         var results2 = [Int]()
-        var completions2 = [Subscribers.Completion<AnError>]()
+        var completions2 = [Subscribers.Completion<TestError>]()
         
         var results3 = [Int]()
-        var completions3 = [Subscribers.Completion<AnError>]()
+        var completions3 = [Subscribers.Completion<TestError>]()
         
         
         subject
@@ -191,32 +190,32 @@ final class ReplaySubjectTests: XCTestCase {
                 receiveCompletion: { completions1.append($0) },
                 receiveValue: { results1.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject
             .sink(
                 receiveCompletion: { completions2.append($0) },
                 receiveValue: { results2.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject
             .sink(
                 receiveCompletion: { completions3.append($0) },
                 receiveValue: { results3.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
         subject.send(3)
         
         XCTAssertEqual(results1, [1, 2])
-        XCTAssertEqual(completions1, [.failure(.someError)])
+        XCTAssertEqual(completions1, [.failure(.test)])
         
         XCTAssertEqual(results2, [1, 2])
-        XCTAssertEqual(completions2, [.failure(.someError)])
+        XCTAssertEqual(completions2, [.failure(.test)])
         
         XCTAssertEqual(results3, [1, 2])
-        XCTAssertEqual(completions3, [.failure(.someError)])
+        XCTAssertEqual(completions3, [.failure(.test)])
     }
     
     func test_valueAndCompletionPreSubscribe() {
@@ -224,18 +223,17 @@ final class ReplaySubjectTests: XCTestCase {
         
         subject.send(1)
         subject.send(completion: .finished)
-        
-        var results1 = [Int]()
+
         var completed = false
         
         subject
             .sink(
                 receiveCompletion: { _ in completed = true },
-                receiveValue: { results1.append($0) }
+                receiveValue: { [weak self] in self?.results.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
-        XCTAssertEqual(results1, [1])
+        XCTAssertEqual(results, [1])
         XCTAssertTrue(completed)
     }
     
@@ -245,39 +243,37 @@ final class ReplaySubjectTests: XCTestCase {
         subject.send(1)
         subject.send(completion: .finished)
         subject.send(2)
-        
-        var results1 = [Int]()
+
         var completed = false
         
         subject
             .sink(
                 receiveCompletion: { _ in completed = true },
-                receiveValue: { results1.append($0) }
+                receiveValue: { [weak self] in self?.results.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
-        XCTAssertEqual(results1, [1])
+        XCTAssertEqual(results, [1])
         XCTAssertTrue(completed)
     }
     
     func test_noValuesReplayed_postError() {
-        let subject = ReplaySubject<Int, AnError>(bufferSize: 1)
+        let subject = ReplaySubject<Int, TestError>(bufferSize: 1)
         
         subject.send(1)
-        subject.send(completion: .failure(.someError))
+        subject.send(completion: .failure(.test))
         subject.send(2)
-        
-        var results1 = [Int]()
+
         var completed = false
         
         subject
             .sink(
                 receiveCompletion: { _ in completed = true },
-                receiveValue: { results1.append($0) }
+                receiveValue: { [weak self] in self?.results.append($0) }
             )
-            .store(in: &subscriptions)
+            .store(in: cancellable)
         
-        XCTAssertEqual(results1, [1])
+        XCTAssertEqual(results, [1])
         XCTAssertTrue(completed)
     }
     
@@ -288,21 +284,22 @@ final class ReplaySubjectTests: XCTestCase {
         subject.send(2)
         subject.send(3)
         subject.send(4)
-        
-        var results = [Int]()
+
         var completed = false
         
         let subscriber = AnySubscriber<Int, Never>(
-            receiveSubscription: { subscription in
-                self.demandSubscription = subscription
+            receiveSubscription: { [weak self] subscription in
+                self?.demandSubscription = subscription
                 subscription.request(.max(3))
             },
-            receiveValue: { results.append($0); return .none },
+            receiveValue: { [weak self] in
+                self?.results.append($0)
+                return .none
+            },
             receiveCompletion: { _ in completed = true }
         )
         
-        subject
-            .subscribe(subscriber)
+        subject.subscribe(subscriber)
         
         XCTAssertEqual(results, [1, 2, 3])
         XCTAssertFalse(completed)
@@ -324,7 +321,10 @@ final class ReplaySubjectTests: XCTestCase {
         
         let subscriber = AnySubscriber<String, Never>(
             receiveSubscription: { $0.request(.max(1)) },
-            receiveValue: { results.append($0); return .none },
+            receiveValue: {
+                results.append($0)
+                return .none
+            },
             receiveCompletion: { completions.append($0) }
         )
         
@@ -407,18 +407,6 @@ final class ReplaySubjectTests: XCTestCase {
                 }
             }
         }
-    }
-    
-}
-
-// MARK: - AnError
-
-private extension ReplaySubjectTests {
-    
-    enum AnError: Error, Equatable {
-        
-        case someError
-        
     }
     
 }
